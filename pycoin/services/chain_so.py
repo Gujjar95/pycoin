a3c9601356a47d01b95f392df2c980ce894650a4
gujjar95 <viksingh98@gmail.com>
2018-02-22 13:21:15 +0530

    Chain so spendable exact

--- pycoin/services/chain_so.py
+++ pycoin/services/chain_so.py
@@ -27,22 +27,40 @@ class ChainSoProvider(object):
     def base_url(self, method, args):
         return "https://chain.so/api/v2/%s/%s/%s" % (method, self.network_path, args)
 
-    def spendables_for_address(self, address):
+    def spendables_for_address(self, address, amount = None):
         """
         Return a list of Spendable objects for the
         given bitcoin address.
         """
-        spendables = []
-        r = json.loads(urlopen(self.base_url('get_tx_unspent', address)).read().decode("utf8"))
+        if amount == None :
+            spendables = []
+            r = json.loads(urlopen(self.base_url('get_tx_unspent', address)).read().decode("utf8"))
+            for u in r['data']['txs']:
+                coin_value = int(float(u['value']) * 100000000)
+                script = h2b(u["script_hex"])
+                previous_hash = h2b_rev(u["txid"])
+                previous_index = u["output_no"]
+                spendables.append(Spendable(coin_value, script, previous_hash, previous_index))
+            return spendables
+        else :
+            spendables = []
+            r = json.loads(urlopen(self.base_url('get_tx_unspent', address)).read().decode("utf8"))
+            list_spend = r['data']['txs']
+            total_amount = 0
+            if len(list_spend) == 0:
+                raise Exception("No spendable outputs found")
+            unspents = sorted(list_spend, key=lambda d: d['value'], reverse = True)
+            for u in unspents:
+                coin_value = int(float(u['value']) * 100000000)
+                script = h2b(u["script_hex"])
+                previous_hash = h2b_rev(u["txid"])
+                previous_index = u["output_no"]
+                spendables.append(Spendable(coin_value, script, previous_hash, previous_index))
+                total_amount = total_amount + coin_value
+                if total_amount >= amount:
+                    break
+            return [spendables, total_amount]
 
-        for u in r['data']['txs']:
-            coin_value = int(float(u['value']) * 100000000)
-            script = h2b(u["script_hex"])
-            previous_hash = h2b_rev(u["txid"])
-            previous_index = u["output_no"]
-            spendables.append(Spendable(coin_value, script, previous_hash, previous_index))
-
-        return spendables
 
     def tx_for_tx_hash(self, tx_hash):
         "Get a Tx by its hash."
